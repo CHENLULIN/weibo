310f6c47f9c55c547fa87bec490834eaac47f0a8
林宇东 <306073100@qq.com>
2019-07-03 12:08:41 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<title>@yield('title', 'Weibo') - Laravel 新手入门教程</title>
-	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item"><a class="nav-link" href="/help">帮助1</a></li>
           <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
         </ul>
       </div>

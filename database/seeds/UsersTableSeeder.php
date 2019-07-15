2a8e9d75057649059617f9bc5b5a8ebe0d394f92
林宇东 <306073100@qq.com>
2019-07-15 15:41:43 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(200)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+        $user = User::find(1);
+        $user->name = "Lin yu dong";
+        $user->email = "306073100@qq.com";
+        $user->save();
+    }
+}

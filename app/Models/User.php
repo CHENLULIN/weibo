00af0f5349c6818bef4fddeb66f21e95f8f6cdfe
林宇东 <306073100@qq.com>
2019-07-18 03:29:35 +0000

    粉丝关注、

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,53 @@ class User extends Authenticatable
     {
     	return $this->statuses()->orderBy('created_at', 'desc');
     }
+	
+	/**
+	 * 获取粉丝列表
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+    public function followers()
+    {
+    	return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+	
+	/**
+	 * 获取用户关注人列表
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+    public function followings()
+    {
+    	return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+	
+	/**
+	 * 关注
+	 * @param $user_ids
+	 */
+    public function follow($user_ids)
+    {
+    	if ( ! is_array($user_ids)) {
+    	    $user_ids = compact('user_ids');
+	    }
+	    
+	    $this->followings()->sync($user_ids, false);
+    }
+	
+	/**
+	 * 取消关注
+	 * @param $user_ids
+	 */
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        
+        $this->followings()->detach($user_ids);
+    }
+    
+    public function isFollowings($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }

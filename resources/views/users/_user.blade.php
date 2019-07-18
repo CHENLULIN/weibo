fff581c8f3ee541fc2b9146a059b1ae91c3a4f91
林宇东 <306073100@qq.com>
2019-07-18 06:40:59 +0000

    关注按钮

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,7 +1,6 @@
 <div class="list-group-item">
     <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width="32">
-    <a href="{{ route('users.show', $user) }}"></a>
-    {{ $user->name }}
+    <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
     @can('destroy', $user)
         <form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right" onsubmit="return confirm('您确定要删除该用户吗？')">
             {{ csrf_field() }}
